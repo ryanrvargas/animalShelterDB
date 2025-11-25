e516c49e1f289f04239980ec590d8a122377efd9
Ryan R Vargas <ryanr99@live.com>
2025-11-25 09:40:34 -0500

    Add animal search and shot history features
    Introduces find_animal.html and retrieve_animal.php for searching animals by name and displaying their age and species. Adds two-table-join.html and two-table-join.php to allow searching animals and displaying their shot history using a join between Animal and ShotHistory tables.

---
+++ retrieve_animal.php
@@ -0,0 +1,63 @@
+<?php
+	ini_set('display_errors', 1);
+//Name: 
+/*This code assumes user input is valid and correct only for demo purposes - it does NOT validate form data.*/
+	if(isset($_GET['submit'])) { //Form was submitted
+		$name = $_GET["name"];
+		$name = '%'.$name.'%'; // . is the concatenation operator for PHP strings
+		try{
+			require_once('../pdo_connect.php'); //adjust the relative path as necessary to find your connect file
+			$sql = 'SELECT AnimalName, Species, AnimalDOB
+					FROM Animal
+					WHERE AnimalName LIKE ?
+					ORDER BY AnimalDOB ASC';
+			$stmt = $dbc->prepare($sql);
+			$stmt->bindParam(1, $name);
+			$stmt->execute();	
+		} catch (PDOException $e){
+			echo $e->getMessage();
+		}	
+		$affected = $stmt->rowCount();
+		if ($affected == 0){
+			echo "We could not find a name matching that description. Please try again.";
+			exit;
+		}	
+		else {
+			$result = $stmt->fetchAll();
+		}
+	} //end isset
+	else {
+		echo "<h2>You have reached this page in error</h2>";
+		exit;
+	}
+    $today = new DateTime();
+	
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Animal Shelter</title>
+	<meta charset ="utf-8"> 
+</head>
+<body>
+	<h2> Animals Found: </h2>
+	<table border='1'>
+		<tr>
+			<th>Name</th>
+			<th>Species</th>
+			<th>Age</th>
+		</tr>
+	<?php foreach($result as $row) {
+        $dob = new DateTime($row['AnimalDOB']);
+        $age = $today->diff($dob)->y;   // full years difference
+
+		echo "<tr>";
+		echo "<td>".$row['AnimalName']."</td>";
+		echo "<td>".$row['Species']."</td>";
+		echo "<td>".$age."</td>";
+		echo "</tr>";
+	}?> 
+	</table>
+</body>
+</html>
+</html>
